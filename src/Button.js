fc9cc9d356172f3b2de5ded9e130eb9173be0088
Nicolas CHAUVET <spidoche@NicoMac.local>
2020-10-03 12:10:03 +0900

    move array data to component state

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Button extends React.Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {foodList : [ '김밥', '볶음밥', '만두국', '칼국수', '샌드위치', '샐러드', '라면', '베트남국수', '돈까스', '제육덮밥' ]}
+    }
+    
     clickHandler = () => {
 
-        let arr_value = [ '김밥', '볶음밥', '만두국', '칼국수', '샌드위치', '샐러드', '라면', '베트남국수', '돈까스', '제육덮밥' ];
+        let arr_value = this.state.foodList;
         let value_food =  Math.floor( Math.random() * 10 ) % arr_value.length;
         let result = arr_value[value_food];
 
